5b395ab6f772b48de6835112f57c86837018e185
JuanAlbornoz <juanleotak@gmail.com>
2023-03-09 18:20:24 -0500

    Modo Oscuro Agregado

--- app/src/main/java/pe/com/rompecabezascanario/ActividadIngreso.kt
+++ app/src/main/java/pe/com/rompecabezascanario/ActividadIngreso.kt
@@ -7,6 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
+import com.google.android.material.switchmaterial.SwitchMaterial
 import pe.com.rompecabezascanario.utilidad.Util
 
 class ActividadIngreso : AppCompatActivity() {
@@ -21,12 +25,21 @@ class ActividadIngreso : AppCompatActivity() {
         setContentView(R.layout.actividad_ingreso)
 
         //declarando los controles
+        val swDarkMode = findViewById<SwitchMaterial>(R.id.swDarkMode)
         val txtUsu=findViewById<EditText>(R.id.txtInputUsu)
         val txtCla=findViewById<EditText>(R.id.txtInputCla)
         val btnIngresar=findViewById<Button>(R.id.btnIngresar)
         val btnSalir=findViewById<Button>(R.id.btnSalir)
 
-
+        //Modo Oscuro
+        swDarkMode.setOnCheckedChangeListener{ _,isSelected ->
+            if (isSelected){
+                enableDarkMode()
+            }else{
+                disableDarkMode()
+            }
+        }
+        
         //agregamos eventos a los botones
         btnIngresar.setOnClickListener {
             if(txtUsu.getText().toString()==""){
@@ -55,5 +68,13 @@ class ActividadIngreso : AppCompatActivity() {
         btnSalir.setOnClickListener {
             objutilidad.SalirSistema(this)
         }
+    }
+    private fun enableDarkMode(){
+        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+        delegate.applyDayNight()
+    }
+    private fun disableDarkMode(){
+        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+        delegate.applyDayNight()
     }
 }
